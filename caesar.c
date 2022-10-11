283156a51926578a73e8a0242cb503106066bda3
StarWolf-64 <touchnahom@yahoo.com>
2022-10-10 23:03:25 -0400

    started on the convert key function

--- caesar.c
+++ caesar.c
@@ -1,3 +1,17 @@
 #include <studio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "caesar.h"
 
+int convert_key(char *key){
+	int key=atoi(key);//convert the key to a number
+	//iterate through the key
+	for(int i=0;i<strlen(key);i++){
+		if(){
+		}
+	
+	}
+	
+
+
+}
